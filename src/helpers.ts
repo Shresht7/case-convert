8ed7d31bdd49ae3643f9d6790e2c09a66f3d80bb
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-06-21 08:29:26 +0530

    Add Comments

--- src/helpers.ts
+++ src/helpers.ts
@@ -8,11 +8,24 @@
  * @returns an array of words
  */
 export const splitIntoWords = (str: string): string[] => str
-    .replace(/([A-Z])[^A-Z]*?/g, "_$1")
-    .split(/[_\-]/g)
-    .filter(x => x !== '')
+    .replace(/([A-Z])[^A-Z]*?/g, "_$1") // Insert underscore before uppercase letters
+    .split(/[_\-]/g) // Split the string at underscores
+    .filter(x => x !== '') // Remove empty elements
 
-/** Capitalize the first character of the string */
-export const capitalize = (str: string, all: boolean = false) => all ? str.toUpperCase() : str[0].toUpperCase() + str.substring(1)
-/** Uncapitalize the first character of the string */
-export const uncapitalize = (str: string, all: boolean = false) => all ? str.toLowerCase() : str[0].toLowerCase() + str.substring(1)
+/**
+ * Capitalize the first character of the string
+ * @param str String to capitalize
+ * @param all If true, capitalize all characters; if false (default), only capitalize the first character
+ * @returns Capitalized string
+ */
+export const capitalize = (str: string, all: boolean = false): string =>
+    all ? str.toUpperCase() : str[0].toUpperCase() + str.substring(1);
+
+/**
+ * Uncapitalize the first character of the string
+ * @param str String to uncapitalize
+ * @param all If true, uncapitalize all characters; if false (default), only uncapitalize the first character
+ * @returns Uncapitalized string
+ */
+export const uncapitalize = (str: string, all: boolean = false): string =>
+    all ? str.toLowerCase() : str[0].toLowerCase() + str.substring(1);
